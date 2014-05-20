46bdf9927744e6d1aced47d38de9ac72bd2f3d9f
Robert Weber <robert.weber@logrhythm.com>
2014-05-20 11:44:11 -0600

    Wrong setting

--- scripts/travisbuild.sh
+++ scripts/travisbuild.sh
@@ -2,12 +2,11 @@
 set -e
 
 NAME=MurMurHash
-export CMAKE_CXX_COMPILER=g++
 
 rm -f  CMakeCache.txt
 cd 3rdparty
 unzip -u gtest-1.7.0.zip
 cd ..
-cmake -DCMAKE_CXX_COMPILER_ARG1:STRING=' -fPIC -Ofast -m64 -Wl,-rpath -Wl,/usr/local/probe/lib -Wl,-rpath -Wl,/usr/local/probe/lib64 ' -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_CXX_COMPILER=/usr/local/probe/bin/g++
+cmake -DCMAKE_CXX_COMPILER_ARG1:STRING=' -fPIC -Ofast -m64 -Wl,-rpath -Wl,/usr/local/probe/lib -Wl,-rpath -Wl,/usr/local/probe/lib64 ' -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_CXX_COMPILER=g++
 make
 ./UnitTestRunner
