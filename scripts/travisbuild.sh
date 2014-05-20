66f2bc89c7ef9df5b672651fe2787716e8cefdbf
Robert Weber <robert.weber@logrhythm.com>
2014-05-20 11:37:28 -0600

    A script for travis builds

---
+++ scripts/travisbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+NAME=MurMurHash
+
+rm -f  CMakeCache.txt
+cd 3rdparty
+unzip -u gtest-1.7.0.zip
+cd ..
+cmake -DCMAKE_CXX_COMPILER_ARG1:STRING=' -fPIC -Ofast -m64 -Wl,-rpath -Wl,/usr/local/probe/lib -Wl,-rpath -Wl,/usr/local/probe/lib64 ' -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_CXX_COMPILER=/usr/local/probe/bin/g++
+make
+./UnitTestRunner
